5e9b8fa54f28f555bff2c84b6820f6b2c73f2fa1
Дмитрий Лазуков <lazukov87@yandex.ru>
2024-04-14 18:50:42 +0500

    снова убрали ожидание

--- Selenium_tests_Lazukov/obj/Debug/net6.0/Selenium_tests_Lazukov.AssemblyInfo.cs
+++ Selenium_tests_Lazukov/obj/Debug/net6.0/Selenium_tests_Lazukov.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Selenium_tests_Lazukov")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+849ce9b914ade78c29da7224f2a4ba1c29e1680f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Selenium_tests_Lazukov")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Selenium_tests_Lazukov")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
